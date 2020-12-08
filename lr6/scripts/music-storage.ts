8a494d53304900c2e28d7efea030b40e7e20f9fb
DESKTOP-E22R50E\Sanqer <daredevilis25@gmail.com>
2020-12-08 04:52:36 +0300

    LR6 - v.0.2

--- lr6/scripts/music-storage.ts
+++ lr6/scripts/music-storage.ts
@@ -3,11 +3,37 @@ import Dom from "./dom.js";
 export class MusicStorage {
     input: HTMLInputElement;
     placeHolder: HTMLDivElement;
+    files: [number, File][];
+    index: number;
 
     constructor() {
+        this.index = 0;
+        this.files = [];
         this.input = Dom.select('input[name="musicChooser"]');
         this.placeHolder = Dom.select('#playlist');
         this.input.addEventListener('change', this.onFileInputChange.bind(this));
+    }
+
+    async next(this: MusicStorage): Promise<[File, ArrayBuffer] | null> {
+        if (this.files.length === 0) {
+            return null;
+        }
+        if (this.index >= this.files.length) {
+            this.index = 0;
+        }
+
+        try {
+            let file = this.files[this.index++][1];
+            let arrayBuffer = await MusicStorage.readFileAsync(file);
+            return [file, arrayBuffer];
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    }
+
+    getFile(this: MusicStorage): File {
+        return this.input.files![0];
     }
 
     private onFileInputChange(this: MusicStorage, e: Event): void {
@@ -17,16 +43,42 @@ export class MusicStorage {
             return;
         }
 
+        this.files = [];
         this.placeHolder.innerHTML = "";
         for (let file of files) {
-            this.placeHolder.appendChild(MusicStorage.makeItem(file));
+            let pos = this.files.length;
+            this.files.push([pos, file]);
+            this.placeHolder.appendChild(this.makeItem(file, pos));
         }
+        console.log(this.files);
     }
 
-    private static makeItem(file: File): HTMLDivElement {
+    private makeItem(file: File, index: number): HTMLDivElement {
         let div = document.createElement('div');
-        div.innerText = file.name;
-        div.classList.add('btn', 'btn-primary', 'd-block', 'm-1');
+        let p = document.createElement('p');
+        let deleteButton = document.createElement('button');
+        div.classList.add('border', 'm-2');
+        p.innerText = file.name;
+        deleteButton.innerText = "Delete";
+        deleteButton.classList.add('btn', 'btn-primary', 'd-block', 'm-1');
+        deleteButton.addEventListener('click', () => {
+            this.files.splice(this.files.findIndex(value => value[0] === index) , 1);
+            this.placeHolder.removeChild(div);
+            console.log(this.files);
+        });
+        div.appendChild(p);
+        div.appendChild(deleteButton);
         return div;
+    }
+
+    private static async readFileAsync(file: File): Promise<ArrayBuffer> {
+        return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+            reader.onload = () => {
+                resolve(reader.result as ArrayBuffer);
+            };
+            reader.onerror = reject;
+            reader.readAsArrayBuffer(file);
+        })
     }
 }
--- lr6/scripts/script.ts
+++ lr6/scripts/script.ts
@@ -1,8 +1,103 @@
 import {MusicStorage} from "./music-storage.js";
+import Dom from "./dom.js";
 
 class MyClass {
+    context: AudioContext;
+    source?: AudioBufferSourceNode;
+    sourceBuffer?: AudioBuffer;
+    gainNode: GainNode;
+    loading: boolean;
+
+    playing: boolean;
+    offset: number;
+    playPressedTime?: number;
+
     constructor(private musicStorage: MusicStorage) {
+        this.context = new AudioContext();
+        this.loading = false;
+        this.gainNode = this.context.createGain();
+        this.gainNode.gain.value = 0.1;
+        this.gainNode.connect(this.context.destination);
+
+        this.playing = false;
+        this.offset = 0;
+
+        let playButton = Dom.select<HTMLButtonElement>('button#play');
+        playButton.addEventListener('click', async () => {
+            await this.playPressedHandler();
+        });
+
+        //let volumeUpButton = Dom.select<HTMLButtonElement>('button#volumeUp');
+        //volumeUpButton.addEventListener('click', () => {
+        //    this.source!.start(10);
+            //this.gainNode.gain.value = this.gainNode.gain.value + 0.1;
+            //console.info(this.gainNode.gain.value);
+       // })
     }
+
+    private async loadNext(this: MyClass): Promise<boolean> {
+        let fileAndBuffer = await this.musicStorage.next();
+        if (fileAndBuffer == null) {
+            console.log("couldn't load file for playing");
+        } else {
+            let file = fileAndBuffer[0];
+            let arrayBuffer = fileAndBuffer[1];
+            try {
+                this.sourceBuffer = await this.context.decodeAudioData(arrayBuffer);
+                return true;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        return false;
+    }
+
+    private async playPressedHandler(this: MyClass): Promise<void> {
+        if (this.loading) {
+            console.log("currently loading file...");
+            return;
+        }
+        this.loading = true;
+        if (this.sourceBuffer == null) {
+            let success = await this.loadNext();
+            if (!success) {
+                this.loading = false;
+                return;
+            }
+        }
+
+        this.play();
+        this.loading = false;
+    }
+
+    private play(this: MyClass): void {
+        if (this.sourceBuffer == null) {
+            console.error('buffer was null');
+            return;
+        }
+
+        if (this.playing) {
+            this.source?.stop();
+            this.offset = (Date.now() - (this.playPressedTime ?? Date.now())) / 1000;
+            this.playing = false;
+        } else {
+            let source = this.context.createBufferSource();
+            source.buffer = this.sourceBuffer;
+
+            source.connect(this.gainNode);
+            source.start(0, this.offset);
+
+            this.playPressedTime = Date.now();
+            this.source = source;
+            this.playing = true;
+        }
+    }
+
+    private onEndHandler(this: MyClass): void {
+
+    }
+
 }
 
 new MyClass(new MusicStorage());
+
